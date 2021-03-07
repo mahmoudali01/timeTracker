eb048900cd93b4b16e1f074e2ccaffda466de993
mahmoud <mahmoudali2911@outlook.com>
2021-03-07 05:19:48 +0200

    fixing app structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:andreApp/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import 'app/sign-in/landing_page.dart';
+import 'app/landing_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
